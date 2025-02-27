bee35ae15f68965cb5bf5e3d5eec2bf35d8c91c7
Gabriel Henrique <gabidelnovaes@gmail.com>
2025-02-27 20:51:29 -0300

    Cleaning code

--- src/main/java/com/gabrielcostanovaes/main/GetTitleInfo.java
+++ src/main/java/com/gabrielcostanovaes/main/GetTitleInfo.java
@@ -1,4 +1,34 @@
 package com.gabrielcostanovaes.main;
 
-public class GetTitleInfo {
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gabrielcostanovaes.inicializaAPI.InicializaAPI;
+
+import java.io.IOException;
+
+public class GetTitleInfo extends InicializaAPI{
+
+    public String titleInfo(String titleName) throws IOException, InterruptedException {
+        InicializaAPI inicializaAPI = new InicializaAPI();
+        ObjectMapper jackson = new ObjectMapper();
+
+        String TitleJson = inicializaAPI.getTitle(titleName);
+        JsonNode TitleJsonNode = jackson.readTree(TitleJson)
+                .get("items")
+                .get(0)
+                .get("volumeInfo");
+
+        String Author = TitleJsonNode.get("authors").get(0).asText();
+        String pageCount = TitleJsonNode.get("pageCount").asText();
+        String categories = TitleJsonNode.get("categories").get(0).asText();
+        String description = TitleJsonNode.get("description").asText();
+
+        return "Autores: " + Author + "\n"
+                + "Quantidade de páginas: "+ pageCount + "\n"
+                + "Categorias: " + categories + "\n"
+                + "Descrição: " + description + "\n" ;
+
+    }
+
+
 }
--- src/main/java/com/gabrielcostanovaes/main/Main.java
+++ src/main/java/com/gabrielcostanovaes/main/Main.java
@@ -9,28 +9,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        
-        InicializaAPI inicializaAPI = new InicializaAPI();
+
         Scanner input = new Scanner(System.in);
-        ObjectMapper jackson = new ObjectMapper();
+        GetTitleInfo getTitleInfo = new GetTitleInfo();
 
         System.out.println("Digite o nome do livro:");
         String titleName = input.nextLine();
 
-        String TitleJson = inicializaAPI.getTitle(titleName);
-        JsonNode TitleJsonNode = jackson.readTree(TitleJson)
-                .get("items")
-                .get(0)
-                .get("volumeInfo");
-
-        String Author = TitleJsonNode.get("authors").get(0).asText();
-        String pageCount = TitleJsonNode.get("pageCount").asText();
-        String categories = TitleJsonNode.get("categories").get(0).asText();
-        String description = TitleJsonNode.get("description").asText();
-
-        System.out.println("Autores: "+Author);
-        System.out.println("Numéro de páginas: "+ pageCount);
-        System.out.println("Classificação: "+categories);
-        System.out.println("Descrição: "+description);
+        System.out.println(getTitleInfo.titleInfo(titleName));
     }
 }
