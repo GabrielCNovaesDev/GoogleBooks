a44ad4634a3c9c151493492adadccc517f88a9b3
Gabriel Henrique <gabidelnovaes@gmail.com>
2025-02-27 20:55:03 -0300

    Adjusting Bugs

--- src/main/java/com/gabrielcostanovaes/main/Main.java
+++ src/main/java/com/gabrielcostanovaes/main/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException{
 
         Scanner input = new Scanner(System.in);
         GetTitleInfo getTitleInfo = new GetTitleInfo();
